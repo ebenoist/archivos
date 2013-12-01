9ee03e12aea5b82bca43064d366d242b05890c80
Erik Benoist <ebenoist@groupon.com>
2013-12-01 11:45:19 -0600

    updating gems, trying to shore this up

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require "capistrano/bundler"
+
 set :application, "archvios"
 set :repo_url, "git@github.com:ebenoist/archivos.git"
 
@@ -6,10 +8,17 @@ set :scm, :git
 
 set :format, :pretty
 set :log_level, :debug
-set :pty, true
+set :pty, false
+set :deploy_via, :remote_cache
 
 set :linked_files, %w{config/aws.yml}
 set :linked_dirs, %w{log tmp/pids}
+set :bundle_roles, :app
+set :bundle_without, %w{development test}.join(' ')
+set :bundle_bins, %w(gem rake rails)
+set :bundle_gemfile, -> { release_path.join('Gemfile') }
+set :bundle_flags, '--deployment '
+set :bundle_dir, -> { shared_path.join('bundle') }
 
 set :keep_releases, 5
 
@@ -17,22 +26,22 @@ namespace :deploy do
   desc "Start application"
   task :start do
     on roles(:app) do
-      execute :rake, start
+      execute("cd #{current_path} && bundle exec rake start")
     end
   end
 
   desc "Stop application"
   task :start do
     on roles(:app) do
-      execute :rake, stop
+      execute("cd #{current_path} && bundle exec rake stop")
     end
   end
 
   desc "Restart application"
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :rake, stop
-      execute :rake, start
+      execute("cd #{current_path} && bundle exec rake stop")
+      execute("cd #{current_path} && bundle exec rake start")
     end
   end
 end
