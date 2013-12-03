7339764f0803c30b9e531ca9b0340df989d9e38d
Erik Benoist <ebenoist@gmail.com>
2013-12-02 19:41:09 -0600

    it can be deployed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
 require "bundler/capistrano"
 require "capistrano/ext/multistage"
+require "rvm/capistrano"
 
 set :application, "archvios"
-set :repo_url, "git@github.com:ebenoist/archivos.git"
+set :repository, "git@github.com:ebenoist/archivos.git"
 set :stages, %w(production)
 
 set :deploy_to, "/home/ubuntu/archivos"
@@ -16,6 +17,18 @@ set :deploy_via, :remote_cache
 set :linked_files, %w{config/aws.yml}
 set :linked_dirs, %w{log tmp/pids}
 set :keep_releases, 5
+set :user, "ubuntu"
+set :use_sudo, false
+
+#SET UP RVM
+set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
+set :rvm_install_ruby_params, '--1.9'      # for jruby/rbx default to 1.9 mode
+set :rvm_install_pkgs, %w[libyaml openssl] # package list from https://rvm.io/packages
+set :rvm_install_ruby_params, '--with-opt-dir=/usr/local/rvm/usr' # package support
+
+before 'deploy:setup', 'rvm:install_rvm'   # install RVM
+before 'deploy:setup', 'rvm:install_ruby'  # install Ruby and create gemset, or:
+before 'deploy:setup', 'rvm:create_gemset' # only create gemset
 
 namespace :deploy do
   desc "Start application"
