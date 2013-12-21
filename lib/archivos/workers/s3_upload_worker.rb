e8e9543b8bf21c76928fd81f85c3bed62b31a4a6
Erik Benoist <ebenoist@gmail.com>
2013-12-21 16:13:02 -0600

    refactor

--- lib/archivos/workers/s3_upload_worker.rb
+++ lib/archivos/workers/s3_upload_worker.rb
@@ -1,28 +1,26 @@
-require "archivos/api/models/media"
+require "archivos/models/media"
 require "archivos/s3_client"
 
-module Archivos
-  class S3UploadWorker
-    include Sidekiq::Worker
+class S3UploadWorker
+  include Sidekiq::Worker
 
-    def perform(job)
-      object = S3Client.upload_file!(job["file"], calculate_file_name(job))
+  def perform(job)
+    object = S3Client.upload_file!(job["file"], calculate_file_name(job))
 
-      media = Media.find(job["id"])
-      media.update_attributes({ public_uri: object.public_url.to_s })
-    end
+    media = Media.find(job["id"])
+    media.update_attributes({ public_uri: object.public_url.to_s })
+  end
 
-    def calculate_file_name(job)
-      "#{job["order_code"]}_#{job["file_name"]}"
-    end
+  def calculate_file_name(job)
+    "#{job["order_code"]}_#{job["file_name"]}"
+  end
 
-    def connection
-      @connection ||= AWS::S3.new(aws_credentials)
-    end
+  def connection
+    @connection ||= AWS::S3.new(aws_credentials)
+  end
 
-    def aws_credentials
-      @aws_credentials ||= YAML.load_file(Archivos.root + "/config/aws.yml")
-    end
+  def aws_credentials
+    @aws_credentials ||= YAML.load_file(Archivos.root + "/config/aws.yml")
   end
 end
 
---
+++ spec/unit/workers/s3_upload_worker_spec.rb
@@ -0,0 +1,43 @@
+require "archivos/workers/s3_upload_worker"
+
+describe S3UploadWorker do
+  before(:each) do
+    Sidekiq::Testing.inline!
+
+    @media = Media.new
+    @media.save!
+
+    @file_fixture = Archivos.root + "/spec/fixtures/image_one.jpg"
+    @job_description = { id: @media.id.to_s, order_code: "foo", file: @file_fixture, file_name: "bar.jpg" }
+
+    @s3_object = double(:s3_object, { public_url: "foo.local/image.img" })
+    S3Client.stub(:upload_file!).and_return(@s3_object)
+  end
+
+  it "uses the s3 client to upload the file in the job description" do
+    file = Archivos.root + "/spec/fixtures/image_two.jpg"
+    order_code = "order"
+    file_name = "actual_file_name.jpg"
+
+    @job_description[:file] = file
+    @job_description[:order_code] = order_code
+    @job_description[:file_name] = file_name
+
+    expected_file_name = "#{order_code}_#{file_name}"
+    S3Client.should_receive(:upload_file!).with(file, expected_file_name).and_return(@s3_object)
+
+    S3UploadWorker.perform_async(@job_description)
+  end
+
+  it "adds the public uri to the media object after uploading" do
+    uri = URI("http://amazonsupr.internet.com/my-pixs.jpg")
+    s3_object = double(:s3_object, { public_url: uri })
+    S3Client.stub(:upload_file!).and_return(s3_object)
+
+    S3UploadWorker.perform_async(@job_description)
+
+    @media.reload
+    expect(@media.public_uri).to eq(uri.to_s)
+  end
+end
+
